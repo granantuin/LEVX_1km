a939b2178dfaa3f6c480d3ef492f82f4fdcbe1ca
granantuin <49106818+granantuin@users.noreply.github.com>
2022-09-02 11:29:30 +0200

    Update operational_LEVX_1km_st.py

--- operational_LEVX_1km_st.py
+++ operational_LEVX_1km_st.py
@@ -27,15 +27,21 @@ vis_ml = (pd.DataFrame(alg["pipe"].predict_proba(model_x_var))).iloc[:,0].map("{
 
 #open algorithm precipitation d0
 alg = pickle.load(open("algorithms/prec_LEVX_1km_d0.al","rb"))
-
-#load raw meteorological model and get model variables
-meteo_model = get_meteogalicia_model_1Km (alg["coor"])
  
 #select x _var
 model_x_var = meteo_model[:24][alg["x_var"]]
 
 #forecast machine learning  horizontal visibility meters
 prec_ml = (pd.DataFrame(alg["pipe"].predict_proba(model_x_var))).iloc[:,0].map("{:.0%}".format).values
+
+#open algorithm precipitation d0
+alg = pickle.load(open("algorithms/skyc1_LEVX_1km_d0.al","rb"))
+ 
+#select x _var
+model_x_var = meteo_model[:24][alg["x_var"]]
+
+#forecast machine learning  horizontal visibility meters
+skyc1_ml = alg["pipe"].predict(model_x_var)
 
 #show results prec visibility fog cloud cover
 st.write("#### **Machine learning results (Horizontal visibility, BR/FG, cloud low layer cover and height) forecast D0**")
@@ -53,7 +59,8 @@ df_for0=pd.DataFrame({"time UTC":meteo_model[:24].index,
 
 df_for0=pd.DataFrame({"time UTC":meteo_model[:24].index,
                      "visibility <=1000m (prob)":vis_ml,
-                     "Precipitation (prob)":prec_ml,})
+                     "Precipitation (prob)":prec_ml,
+                     "Cloud cover":skyc1_ml,})
 
 df_all=pd.concat([df_for0.set_index("time UTC"),metar_df],axis=1).reset_index()
 df_all=df_all.rename(columns={"index": "Time UTC"})
