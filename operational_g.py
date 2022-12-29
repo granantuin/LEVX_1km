367f15f512d99a880766b8a742d4b63a082687f8
granantuin <49106818+granantuin@users.noreply.github.com>
2022-12-29 11:15:09 +0100

    Update operational_g.py

--- operational_g.py
+++ operational_g.py
@@ -214,7 +214,9 @@ mae_wrf = round(mean_absolute_error(df_res_dropna.spd_o,df_res_dropna.spd_WRF),2
 #print results
 st.write("""
 Wind intensity knots
+
 Reference (48 hours) Mean absolute error meteorological model: 1.35
+
 Reference (48 hours) Mean absolute error machine learning: 0.89
 """)
 
