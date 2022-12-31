787b0340f0a95038eb963498115eeae5167e5001
granantuin <49106818+granantuin@users.noreply.github.com>
2022-12-31 10:34:35 +0100

    Update operational_g.py

--- operational_g.py
+++ operational_g.py
@@ -477,8 +477,8 @@ prob = (np.concatenate((alg["pipe"].predict_proba(model_x_var),alg1["pipe"].pred
 df_prob = (pd.DataFrame(prob,index =alg["pipe"].classes_ ).T.set_index(meteo_model[:48].index))
 df_prob["time"] = meteo_model[:48].index
 st.write("""Probabilistic results""")
-AgGrid(round(df_prob,2)) 
-st.bar_chart(df_prob, x= "time",y = ["<= 1000 m", "> 1000 m"])
+#AgGrid(round(df_prob,2)) 
+st.bar_chart(round(df_prob,2), x= "time",y = "<= 1000 m")
 
 #@title BR or FG
 #open algorithm prec d0 d1
