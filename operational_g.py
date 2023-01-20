226f20d6e1c0f80808a80d66ca09e8b10e88bcf3
granantuin <49106818+granantuin@users.noreply.github.com>
2023-01-20 10:28:04 +0100

    Update operational_g.py

--- operational_g.py
+++ operational_g.py
@@ -379,11 +379,12 @@ plt.grid(True, axis="both")
 st.pyplot(fig)
 
 #show probabilistic results
-prob = round((np.concatenate((alg["pipe"].predict_proba(model_x_var),alg1["pipe"].predict_proba(model_x_var1)),axis =0)),1).transpose()
+prob = (np.concatenate((alg["pipe"].predict_proba(model_x_var),alg1["pipe"].predict_proba(model_x_var1)),axis =0)).transpose()
 df_prob = (pd.DataFrame(prob,index =alg["pipe"].classes_ ).T.set_index(meteo_model[:48].index.map(lambda t: t.strftime('%d-%m %H'))))
 st.write(""" **Gusts probability** """)
 #AgGrid(round(df_prob,2)) 
 fig, ax = plt.subplots(figsize=(10,8))
+df_prob["Gust"] = df_prob["Gust"].round(1)
 df_prob["Gust"].plot(ax=ax, grid=True, kind='bar')
 
 st.pyplot(fig)
